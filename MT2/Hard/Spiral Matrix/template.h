29121a0c170f1d6bdb83b4eb203887bb2f3d8a2c
Daylen1160 <daylen924@gmail.com>
2026-04-11 04:13:09 -0700

    docs: minor doc fix in Spiral Matrix

--- MT2/Hard/Spiral Matrix/template.h
+++ MT2/Hard/Spiral Matrix/template.h
@@ -11,7 +11,7 @@ void spiralOrder(int NUMROWS, int NUMCOLS, int matrix[][NUMCOLS], int result[]);
  * @param[in] NUMROWS    Number of rows in matrix
  * @param[in] NUMCOLS    Number of columns in matrix
  * @param[in] matrix     The input matrix
- * @param[out] result    Spiral order output array (size NUMROWS * NUMCOLS)
+ * @param[in, out] result    Spiral order output array (size NUMROWS * NUMCOLS)
  * @modifies result array
  */
 void spiralOrder(int NUMROWS, int NUMCOLS, int matrix[][NUMCOLS], int result[]) {
