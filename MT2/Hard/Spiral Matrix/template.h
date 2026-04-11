a024c9a28e70e976d50335922b37d04b43051b34
Daylen1160 <daylen924@gmail.com>
2026-04-11 04:13:36 -0700

    docs: minor spacing update in Spiral Matrix

--- MT2/Hard/Spiral Matrix/template.h
+++ MT2/Hard/Spiral Matrix/template.h
@@ -8,10 +8,10 @@ void spiralOrder(int NUMROWS, int NUMCOLS, int matrix[][NUMCOLS], int result[]);
 
 /**
  * @brief Traverse matrix in spiral order and store values in result array
- * @param[in] NUMROWS    Number of rows in matrix
- * @param[in] NUMCOLS    Number of columns in matrix
- * @param[in] matrix     The input matrix
- * @param[in, out] result    Spiral order output array (size NUMROWS * NUMCOLS)
+ * @param[in] NUMROWS       Number of rows in matrix
+ * @param[in] NUMCOLS       Number of columns in matrix
+ * @param[in] matrix        The input matrix
+ * @param[in, out] result   Spiral order output array (size NUMROWS * NUMCOLS)
  * @modifies result array
  */
 void spiralOrder(int NUMROWS, int NUMCOLS, int matrix[][NUMCOLS], int result[]) {
